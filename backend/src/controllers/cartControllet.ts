7d41fbf085c18f36f895025e0ea240cabead8549
gabrielglasser <sga91000@gmail.com>
2025-04-03 17:30:52 -0300

    refatora controle de carrinho para usar tipo de requisição autenticada e ajusta configuração do TypeScript

--- backend/src/controllers/cartControllet.ts
+++ backend/src/controllers/cartControllet.ts
@@ -2,23 +2,24 @@ import { Request, Response } from "express";
 import CartService from "../service/cartService";
 import ApiResponse from "../utils/apiResponse";
 import asyncHandler from "../utils/asyncHandler";
+import { AuthenticatedRequest } from "../types/customRequest";
 import { AddToCartInput, UpdateCartItemInput } from "../models/cartModel";
 
 class CartController {
-  async getCart(req: Request, res: Response) {
-    const userId = req.user!.id;
+  async getCart(req: AuthenticatedRequest, res: Response) {
+    const userId = req.user.id;
     const cart = await CartService.getCartByUserId(userId);
     ApiResponse.success(res, cart);
   }
 
-  async addToCart(req: Request, res: Response) {
-    const userId = req.user!.id;
+  async addToCart(req: AuthenticatedRequest, res: Response) {
+    const userId = req.user.id;
     const cart = await CartService.addToCart(userId, req.body);
     ApiResponse.success(res, cart);
   }
 
-  async updateCartItem(req: Request, res: Response) {
-    const userId = req.user!.id;
+  async updateCartItem(req: AuthenticatedRequest, res: Response) {
+    const userId = req.user.id;
     const cart = await CartService.updateCartItem(
       userId,
       req.params.itemId,
@@ -27,14 +28,14 @@ class CartController {
     ApiResponse.success(res, cart);
   }
 
-  async removeFromCart(req: Request, res: Response) {
-    const userId = req.user!.id;
+  async removeFromCart(req: AuthenticatedRequest, res: Response) {
+    const userId = req.user.id;
     const cart = await CartService.removeFromCart(userId, req.params.itemId);
     ApiResponse.success(res, cart);
   }
 
-  async clearCart(req: Request, res: Response) {
-    const userId = req.user!.id;
+  async clearCart(req: AuthenticatedRequest, res: Response) {
+    const userId = req.user.id;
     const cart = await CartService.clearCart(userId);
     ApiResponse.success(res, cart);
   }
